7748e0adc4527dae147d73ce826ce6933c449ec1
pratik raut <pratikmraut123@gmail.com>
2022-12-04 22:30:18 +0530

    changes in signIn and SignUp page, verification upload update, contact page update

--- app.py
+++ app.py
@@ -3,17 +3,26 @@
 import os
 import json
 from flask import request
-
+import glob
+import cv2
+import numpy as np
+import pyzbar.pyzbar as pyzbar
+import pandas as pd
+import pathlib
 from flask import Flask, render_template, session, redirect
 from functools import wraps
 import pymongo
+
+
+import tika
+tika.initVM()
+from tika import parser
 
 app = Flask(__name__)
 app.secret_key = b'\xcc^\x91\xea\x17-\xd0W\x03\xa7\xf8J0\xac8\xc5'
 
 # Database
 # client = pymongo.MongoClient('localhost', 27017)
-
 # usr = os.environ['voyager']
 # pwd = os.environ['hubbble']
 
@@ -38,6 +47,14 @@ from user import routes
 @app.route('/')
 def home():
   return render_template('home.html')
+
+@app.route('/home1')
+def signUp():
+  return render_template('home1.html')
+
+# @app.route('/home1/')
+# def home1():
+#   return render_template('home1.html')
 
 @app.route('/dashboard/')
 @login_required
@@ -69,19 +86,38 @@ def upload():
 #   return render_template('verification.html')
 
 
-app.config["UPLOAD_DIR"] = "uploads_doc"
+app.config["UPLOAD_DIR"] = "uploads"
 @app.route("/verification/", methods = ["GET", "POST"])
 @login_required
 def verification():
+  
   print(request.method)
+
+  def read_qr_code(filename):
+    image = cv2.imread(os.path.join(app.config['UPLOAD_DIR'], filename))
+
+    decodedObjects = pyzbar.decode(image)
+    for obj in decodedObjects:
+        # print("Type: QRCODE")
+        # print("Data:  b'www.copyassignment.com'")
+        print("Type:", obj.type)
+        print("Data: ", obj.data, "\n")
+  def resume(filename):
+    file_data = parser.from_file(os.path.join(app.config['UPLOAD_DIR'], filename))
+    text = file_data['content']
+    print(text)
+
   if request.method == 'POST':
       file = request.files['file']
       file.save(os.path.join(app.config['UPLOAD_DIR'], file.filename))
+      # print(read_qr_code(file.filename))
+      resume(file.filename)
+
       return render_template("verification.html", msg = "File uplaoded successfully.")
   return render_template("verification.html", msg = "")
 
 
-@app.route('/profile/')
+@app.route('/profile/' , methods = ["GET", "POST"])
 @login_required
 def profile():
   return render_template('profile.html')
@@ -92,5 +128,6 @@ def contact():
   return render_template('contact.html')
 
 
-  if __name__ == "__main__":
-    app.run(debug=True)
+
+if __name__ == "__main__":
+  app.run(debug=True)
